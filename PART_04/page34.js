df591420dde227a18b7269f19a3f5ebe005d309f
AdithyaSurya06 <suryaadithya9849@gmail.com>
2026-03-06 13:44:48 +0530

    updated

--- PART_04/page34.js
+++ PART_04/page34.js
@@ -1,41 +1,45 @@
-import express from "express"
+import express from "express";
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.listen(8080, () => console.log("Server Started"));
 
-const app=express()
-app.set("view engine","ejs")
-app.set("views","views")
-app.listen(8080,()=>console.log("server started")) 
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.urlencoded({extended:true}));
+let users = [
+  { name: "Adithya", email: "adithya@gmail.com", password: "1234" },
+  { name: "Adi", email: "adi@gmail.com", password: "1234" },
+  { name: "surya", email: "surya@gmail.com", password: "1234" },
+];
 
-const users = [
-    {name:"adithya",email:"adithya@gmail.com",password:"1234"},
-    {name:"adi",email:"adi@gmail.com",password:"1234"},
-    {name:"surya",email:"surya@gmail.com",password:"1234"}
-]
-
-app.get("/login",(req,res)=>{
-    res.render("login")
+app.get("/login", (req, res) => {
+  res.render("login", { error: null });
 });
 
-app.post("/login",(req,res)=>{
-     const {email,password} = req.body;
-     const user = users.find((user)=> user.email === email)
-     if(user){
-        if(user.password === password){
-            res.redirect("/")
-        }else{
-            res.redirect("/login")
-        }
-     }
-     else{
-        res.redirect("/login")
-     }
-     res.redirect("/")
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const user = users.find((user) => user.email === email);
+  if (user) {
+    if (user.password === password) {
+      res.redirect("/");
+    } else {
+      res.render("login", { error: "Invalid Password" });
+    }
+  } else {
+    res.render("login", { error: "User not found" });
+  }
+  // res.redirect("/");
 });
 
-app.get("/register",(req,res)=>{
-    res.render("register")
-})
-app.get("/",(req,res)=>{
-    res.render("dashboard",{users})
-})
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  users = [...users, req.body];
+  res.redirect("/");
+});
+
+app.get("/", (req, res) => {
+  res.render("dashboard", { users });
+});
